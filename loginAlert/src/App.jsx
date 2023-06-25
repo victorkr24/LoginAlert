aa8ce7b450553d1ae9500d45661c55c547bb8aa3
victorkr24 <prueba@gmail.com>
2023-06-25 16:24:52 -0600

    Update Link

--- loginAlert/src/App.jsx
+++ loginAlert/src/App.jsx
@@ -26,12 +26,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="www.google.com">Este es un link</a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
